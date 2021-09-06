6183e302120275cfb0a62e49bf8d6fb66333959b
ayhanexe <aykhansab@code.edu.az>
2021-09-06 15:55:19 +0400

    small bug fixed

--- Hotel_Management_System_v2/ConsolePages/Core.cs
+++ Hotel_Management_System_v2/ConsolePages/Core.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Hotel_Management_System_v2.Services;
@@ -12,6 +13,7 @@ namespace Hotel_Management_System_v2.ConsolePages
     {
         public static void Init()
         {
+            Thread.Sleep(2000);
             if (!AuthService.IsLoggedIn)
             {
                 AuthPage();
